dbca3df6828bddef46b980185016572004253e85
baud <chance@houseofbaud.com>
2026-03-07 19:25:44 -0800

    Correct missed enigma-bbs hardcoded directoryw

--- watch.sh
+++ watch.sh
@@ -3,7 +3,7 @@
 ENIGMA_INSTALL_DIR=${ENIGMA_INSTALL_DIR:=$HOME/enigma-bbs}
 
 LOGFILE_PATH=${ENIGMA_INSTALL_DIR}/logs/enigma-bbs.log
-BUNYAN_BINARY_PATH=${ENIGMA_INSTALL_DIR}/enigma-bbs/node_modules/bunyan/bin/bunyan
+BUNYAN_BINARY_PATH=${ENIGMA_INSTALL_DIR}/node_modules/bunyan/bin/bunyan
 
 PATH="$HOME/.local/share/mise/shims:$PATH"
 
